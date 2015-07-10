44f1d363e886f67df6a21da93641d5dd923fdd43
hiogawa <hi.ogawa.zz@gmail.com>
2015-07-10 21:09:56 +0900

    I've given up touching rspec stuff, for now

--- wordCollectorRails/spec/models/category_spec.rb
+++
@@ -1,6 +0,0 @@
-# http://stackoverflow.com/questions/17507416/rspec-doesnt-see-my-model-class-uninitialized-constant-error
-require 'rails_helper'
-
-describe Category do
-  it "has a valid factory"
-end
