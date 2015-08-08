60d74a40e3f20d559c416890d5de9d2c7e83c753
hiogawa <hi.ogawa.zz@gmail.com>
2015-08-08 11:37:37 +0900

    add resource for items

---
+++ railsAPI/spec/controllers/api/v1/items_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Api::V1::ItemsController do
+
+  describe "GET #index" do
+  end
+  
+  describe "GET #show" do
+  end 
+
+  describe "POST #create" do
+  end
+
+  describe "PUT #update" do
+  end
+
+  describe "DELETE #destroy" do
+  end
+
+end
