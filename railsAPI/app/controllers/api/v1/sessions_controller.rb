eabd4416334a3bd147cc098fbb08ee5f3bda25cb
hiogawa <hi.ogawa.zz@gmail.com>
2015-08-08 15:49:57 +0900

    remove devise controller

--- railsAPI/app/controllers/api/v1/sessions_controller.rb
+++ railsAPI/app/controllers/api/v1/sessions_controller.rb
@@ -5,10 +5,9 @@ class Api::V1::SessionsController < ApplicationController
     user = user_info[:email].present? && User.find_by(email: user_info[:email])
     
     if user.valid_password?(user_info[:password])
-      # sign_in(user, store: false)
-      render json: user, status: 200, location: [:api, user]
+      render json: user, status: :ok
     else
-      render json: {errors: "Invalid email or password"}, status: 422
+      render json: {errors: "Invalid email or password"}, status: :unprocessable_entity
     end
   end
 
@@ -16,6 +15,6 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(auth_token: params[:id])
     user.generate_authentication_token!
     user.save
-    head 204
+    head :no_content
   end
 end
