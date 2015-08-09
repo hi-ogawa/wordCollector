d578816b7f9e8510c4a3a7477dbfd3c81bff4ac3
hiogawa <hi.ogawa.zz@gmail.com>
2015-08-09 14:33:33 +0900

    add api version routing spec

---
+++ railsAPI/spec/requests/api_version_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe "API version routing" do
+
+  let(:v1) { {"HTTP_ACCEPT" => "application/vnd.railsAPI.v1"} }
+  let(:v2) { {"HTTP_ACCEPT" => "application/vnd.railsAPI.v2"} }
+
+  context "without version on header" do
+    before(:each) {get "api/items", {}}
+    it "routes to version1 by default" do
+      should have_http_status 200
+    end
+  end
+
+  context "with Accept:application/vnd.railsAPI.v1 on header" do
+    before(:each) {get "api/items", {}, v1}
+    it "routes to version1" do
+      should have_http_status 200
+    end
+  end
+
+  context "with Accept:application/vnd.railsAPI.v2 on header" do
+    before(:each) {get "api/items", {}, v2}
+    it "routes to version1 no matter what is written on header for now" do
+      should have_http_status 200
+    end
+  end
+
+end
