76e6801d58abfe6905f3ac281a280fd7041c77af
hiogawa <hi.ogawa.zz@gmail.com>
2015-08-10 04:40:36 +0900

    change sessions controller api

--- railsAPI/spec/requests/integration_spec.rb
+++ railsAPI/spec/requests/integration_spec.rb
@@ -13,7 +13,7 @@ describe "-- integration API chain --" do
 
 
     puts "login (return auth_token)"
-    post "api/sessions", {session: {email: user_attr[:email], password: user_attr[:password]}}
+    post "api/sessions", {user: {email: user_attr[:email], password: user_attr[:password]}}
     should have_http_status 200
 
 
