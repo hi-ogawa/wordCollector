cec860996414925689e9798b4a9b50342689329b
hiogawa <hi.ogawa.zz@gmail.com>
2015-08-28 02:02:17 +0900

    change yeoman build distination

--- yeomanNgClient/Gruntfile.js
+++ yeomanNgClient/Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: 'dist'
+    dist: '../railsAPI/public'
   };
 
   var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
