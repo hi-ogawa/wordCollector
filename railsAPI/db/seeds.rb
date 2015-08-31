652b8a56e9b1588cea6f80fa282cc1a8862fecfb
hiogawa <hi.ogawa.zz@gmail.com>
2015-08-31 16:14:18 +0900

    not use Amazon S3 on development, fix seed to avoid too many open tempfile bug

--- railsAPI/db/seeds.rb
+++ railsAPI/db/seeds.rb
@@ -6,33 +6,49 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-FactoryGirl.define do
-  factory :user_seed, class: User do
-    password "12345678"
-    password_confirmation "12345678"
+if Rails.env == "development"
+
+  FactoryGirl.define do
+    factory :user_seed, class: User do
+      password "12345678"
+      password_confirmation "12345678"
+    end
   end
-end
 
-FactoryGirl.define do
-  factory :category_seed, class: Category do
-    name        { FFaker::Movie.title }
-    description { FFaker::DizzleIpsum.sentence }
+  FactoryGirl.define do
+    factory :category_seed, class: Category do
+      name        { FFaker::Movie.title }
+      description { FFaker::DizzleIpsum.sentence }
+    end
   end
-end
 
-FactoryGirl.define do
-  factory :item_seed, class: Item do
-    word        { FFaker::DizzleIpsum.words[0,2].join(" ") }
-    sentence    { FFaker::DizzleIpsum.sentence }
-    meaning     { FFaker::DizzleIpsum.sentence }
-    picture     { fixture_file_upload(Rails.root.join('spec', 'pictures', 'test.png'), 'image/png') }
+  FactoryGirl.define do
+    factory :item_seed, class: Item do
+      word        { FFaker::DizzleIpsum.words[0,2].join(" ") }
+      sentence    { FFaker::DizzleIpsum.sentence }
+      meaning     { FFaker::DizzleIpsum.sentence }
+      picture     { fixture_file_upload(Rails.root.join('spec', 'pictures', 'test.png'), 'image/png') }
+    end
   end
+
+
+  user1       = FactoryGirl.create :user_seed, {username: "hiogawa", email: "hiogawa@hiogawa.com"}
+  user2       = FactoryGirl.create :user_seed, {username: "testtest", email: "test@test.com"}
+  categories1 = 10.times.map{FactoryGirl.create :category_seed, user: user1}
+  categories2 = 3.times.map{FactoryGirl.create :category_seed, user: user2}
+
+  itemss1     = categories1.map do |c|
+    20.times.map do 
+      pic = fixture_file_upload(Rails.root.join('spec', 'pictures', 'test.png'), 'image/png')
+      FactoryGirl.create :item_seed, picture: pic, category: c
+      pic.close
+    end
+  end.flatten
+
+  itemss2     = categories2.map do |c|
+    3.times.map do
+      FactoryGirl.create :item_seed, category: c 
+    end
+  end.flatten
+
 end
-
-
-user1       = FactoryGirl.create :user_seed, {username: "hiogawa", email: "hiogawa@hiogawa.com"}
-user2       = FactoryGirl.create :user_seed, {username: "testtest", email: "test@test.com"}
-categories1 = 5.times.map{FactoryGirl.create :category_seed, user: user1}
-categories2 = 3.times.map{FactoryGirl.create :category_seed, user: user2}
-itemss1     = categories1.map{|c| 15.times.map {puts "creates items"; FactoryGirl.create :item_seed, category: c}}.flatten
-itemss2     = categories2.map{|c| 3.times.map {FactoryGirl.create :item_seed, category: c}}.flatten
