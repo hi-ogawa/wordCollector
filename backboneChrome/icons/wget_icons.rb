3b552e857e3acd48032f839880c5aa843b0633b9
hiogawa <hi.ogawa.zz@gmail.com>
2015-09-01 00:02:06 +0900

    chrome: change icons for publishment and manifest

--- backboneChrome/icons/wget_icons.rb
+++ backboneChrome/icons/wget_icons.rb
@@ -3,13 +3,9 @@
 # 3. put the command 
 #    $ ruby wget_icons.rb
 
-# base = 'http://icons.iconarchive.com/icons/iconicon/alpha-magnets/'
-# file = '/Letter-b-icon.png'
-
-base = "http://icons.iconarchive.com/icons/chromatix/keyboard-keys/"
-file = "/letter-uppercase-T-icon.png"
+base = "http://icons.iconarchive.com/icons/iconarchive/red-orb-alphabet/"
+file = "/Letter-V-icon.png"
 sizes = %W[16 48 128]
-
 
 sizes.each do |size|
   `wget #{base + size + file} -O #{size}.png`
