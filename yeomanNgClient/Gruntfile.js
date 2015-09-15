4d8f98d3c921ad101671f0ab460b48ed08f65c91
hiogawa <hi.ogawa.zz@gmail.com>
2015-09-15 10:38:56 +0900

    change frontend build dist dir

--- yeomanNgClient/Gruntfile.js
+++ yeomanNgClient/Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: '../railsAPI/public'
+    dist: 'dist'
   };
 
   var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
