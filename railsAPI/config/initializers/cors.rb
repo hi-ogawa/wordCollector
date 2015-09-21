b88db6ad8d54b7f7ba434ee62fa5b24ad203e62b
Ubuntu <ubuntu@ip-172-31-28-105.ap-northeast-1.compute.internal>
2015-09-21 12:04:45 +0000

    rails: fix cors configuration

--- railsAPI/config/initializers/cors.rb
+++ railsAPI/config/initializers/cors.rb
@@ -1,5 +1,3 @@
-# on production, CORS is not handled by rails, but by nginx (via passenger).
+# On production, CORS is not handled by rails, but by nginx (via passenger).
 # see config/nginx.conf.erb.
-if Rails.env == "development"
-  CORS_CONFIG = YAML.load_file(Rails.root.join("config", "cors.yml"))[Rails.env]
-end
+CORS_CONFIG = YAML.load_file(Rails.root.join("config", "cors.yml"))[Rails.env]
