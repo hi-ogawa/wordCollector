434a4d0479ce18b3e23607671d4e2144fecac2b8
hiogawa <hi.ogawa.zz@gmail.com>
2015-09-21 14:37:22 +0900

    rails: update db seeds not to abuse uploading to s3

--- railsAPI/db/seeds.rb
+++ railsAPI/db/seeds.rb
@@ -27,28 +27,34 @@ if Rails.env == "development"
       word        { FFaker::DizzleIpsum.words[0,2].join(" ") }
       sentence    { FFaker::DizzleIpsum.sentence }
       meaning     { FFaker::DizzleIpsum.sentence }
-      picture     { fixture_file_upload(Rails.root.join('spec', 'pictures', 'test.png'), 'image/png') }
+      # picture     { fixture_file_upload(Rails.root.join('spec', 'pictures', 'test.png'), 'image/png') }
     end
   end
 
 
-  user1       = FactoryGirl.create :user_seed, {username: "hiogawa", email: "hiogawa@hiogawa.com"}
-  user2       = FactoryGirl.create :user_seed, {username: "testtest", email: "test@test.com"}
-  categories1 = 10.times.map{FactoryGirl.create :category_seed, user: user1}
-  categories2 = 3.times.map{FactoryGirl.create :category_seed, user: user2}
+  user_hiogawa       = FactoryGirl.create      :user_seed,         username: "hiogawa",  email: "hiogawa@hiogawa.com"
+  user_testest       = FactoryGirl.create      :user_seed,         username: "testest",  email: "test@test.com"
+  categories_hiogawa = FactoryGirl.create_list :category_seed, 30, user: user_hiogawa
+  categories_testest = FactoryGirl.create_list :category_seed, 30, user: user_testest
 
-  itemss1     = categories1.map do |c|
-    20.times.map do 
-      pic = fixture_file_upload(Rails.root.join('spec', 'pictures', 'test.png'), 'image/png')
-      FactoryGirl.create :item_seed, picture: pic, category: c
-      pic.close
-    end
-  end.flatten
+  items = 30.times.each do
+    pic = fixture_file_upload(Rails.root.join('spec', 'pictures', 'test.png'), 'image/png')
+    FactoryGirl.create :item_seed, picture: pic, category: categories_hiogawa.first
+    pic.close
+  end
 
-  itemss2     = categories2.map do |c|
-    3.times.map do
-      FactoryGirl.create :item_seed, category: c 
-    end
-  end.flatten
+  # itemss1     = categories1.map do |c|
+  #   20.times.map do 
+  #     pic = fixture_file_upload(Rails.root.join('spec', 'pictures', 'test.png'), 'image/png')
+  #     FactoryGirl.create :item_seed, picture: pic, category: c
+  #     pic.close
+  #   end
+  # end.flatten
+
+  # itemss2     = categories2.map do |c|
+  #   3.times.map do
+  #     FactoryGirl.create :item_seed, category: c 
+  #   end
+  # end.flatten
 
 end
